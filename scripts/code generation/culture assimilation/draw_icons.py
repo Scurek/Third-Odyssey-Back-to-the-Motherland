d44d6779f41591c40db25c137b0ab8e27b94399b
Scurek <umajdek@gmail.com>
2022-07-10 14:26:57 +0200

    Added Symmachoi and Skraeling Assimilation to the new system

--- scripts/code generation/culture assimilation/draw_icons.py
+++ scripts/code generation/culture assimilation/draw_icons.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageColor, ImageDraw
 
-
 def interpolate(color1, color2, a):
     return int(color1[0] * (1 - a) + color2[0] * a), \
            int(color1[1] * (1 - a) + color2[1] * a), int(color1[2] * (1 - a) +
@@ -18,31 +17,50 @@ def btree(lst, form, body):
                        btree(lst[:int(len(lst) / 2)], form, body))
 
 
-image = Image.open('to_culture_assimilation.dds')
-imageTI = Image.open('to_tribal_integration.dds')
-width, height = image.size
+modifiers = [{'name': 'to_culture_assimilation',
+              'title': 'Cultural Assimilation',
+              'desc': '[Root.GetCultureAssimilationDesc]'},
+             {'name': 'to_tribal_integration',
+              'title': 'Tribal Integration',
+              'desc': '[Root.GetTribalIntegrationDesc]'},
+             {'name': 'to_culture_assimilation_roman',
+              'title': 'Symmachoi Assimilation',
+              'desc': '[Root.GetSymmachoiAssimilationDesc]'},
+             {'name': 'to_culture_assimilation_norse',
+              'title': 'Skraeling Assimilation',
+              'desc': '[Root.GetSkraelingAssimilationDesc]'}
+             ]
+
+images = [Image.open(f'{modifier["name"]}.dds') for modifier in modifiers]
+
+# image = Image.open('to_culture_assimilation.dds')
+# imageTI = Image.open('to_tribal_integration.dds')
+width, height = images[0].size
 # image = image.convert('RGBA')
-draw = ImageDraw.Draw(image)
-drawTI = ImageDraw.Draw(imageTI)
+
+draws = [ImageDraw.Draw(image) for image in images]
+# draw = ImageDraw.Draw(image)
+# drawTI = ImageDraw.Draw(imageTI)
 
 yellow = ImageColor.getrgb('#ffbb00')
 green = ImageColor.getrgb('#008000')
 
 part_size = int(100 / width)
 
-image.save(f'icons/to_culture_assimilation_0.dds')
-imageTI.save(f'icons/to_tribal_integration_0.dds')
+for index, image in enumerate(images):
+    image.save(f'icons/{modifiers[index]["name"]}_0.dds')
+
+# imageTI.save(f'icons/to_tribal_integration_0.dds')
 fractions = [0]
 
 for i in range(0, width - 4):
     shape = ((2, 28), (2 + i, height))
     color = interpolate(yellow, green, i / width)
-    draw.rectangle(shape, fill=color)
-    drawTI.rectangle(shape, fill=color)
     fraction = int((i + 1) / (width - 3) * 100)
     fractions.append(fraction)
-    image.save(f'icons/to_culture_assimilation_{fraction}.dds')
-    imageTI.save(f'icons/to_tribal_integration_{fraction}.dds')
+    for index, image in enumerate(images):
+        draws[index].rectangle(shape, fill=color)
+        image.save(f'icons/{modifiers[index]["name"]}_{fraction}.dds')
 
 cond = 'check_variable={which=to_assimilation_progress value=%s}'
 body = 'if = {limit={NOT={has_province_modifier=$MODIFIER_BASE_NAME$_%s}} ' \
@@ -52,10 +70,9 @@ form = 'if={limit={%s}%s}else={%s}' % (cond, '%s', '%s')
 
 with open('to_remove_assimilation_modifiers.txt', 'w') as f:
     f.write('to_remove_assimilation_modifiers = {\n')
-    for i in fractions:
-        f.write(f'\tremove_province_modifier = to_culture_assimilation_{i}\n')
-    for i in fractions:
-        f.write(f'\tremove_province_modifier = to_tribal_integration_{i}\n')
+    for index, image in enumerate(images):
+        for i in fractions:
+            f.write(f'\tremove_province_modifier = {modifiers[index]["name"]}_{i}\n')
     f.write('}')
 
 with open('to_set_correct_assimilation_modifier.txt', 'w') as f:
@@ -63,19 +80,21 @@ with open('to_set_correct_assimilation_modifier.txt', 'w') as f:
                                                                   body) + '}')
 
 with open('localisation.txt', 'w') as f:
-    for i in fractions:
-        f.write(f' to_culture_assimilation_{i}:0 \"Cultural Assimilation\"\n')
-        f.write(f' desc_to_culture_assimilation_{i}:0 \"[Root.GetCultureAssimilationDesc]\"\n')
-    for i in fractions:
-        f.write(f' to_tribal_integration_{i}:0 \"Tribal Integration\"\n')
-        f.write(f' desc_to_tribal_integration_{i}:0 \"[Root.GetTribalIntegrationDesc]\"\n')
+    for index, image in enumerate(images):
+        for i in fractions:
+            f.write(f' {modifiers[index]["name"]}_{i}:0 \"{modifiers[index]["title"]}\"\n')
+            f.write(f' desc_{modifiers[index]["name"]}_{i}:0 \"{modifiers[index]["desc"]}\"\n')
+    # for i in fractions:
+    #     f.write(f' to_tribal_integration_{i}:0 \"Tribal Integration\"\n')
+    #     f.write(f' desc_to_tribal_integration_{i}:0 \"[Root.GetTribalIntegrationDesc]\"\n')
 
 with open('modifiers.txt', 'w') as f:
-    for i in fractions:
-        f.write(f'to_culture_assimilation_{i} = {{\n')
-        f.write(f'\tpicture = \"to_culture_assimilation_{i}\"\n')
-        f.write('}\n\n')
-    for i in fractions:
-        f.write(f'to_tribal_integration_{i} = {{\n')
-        f.write(f'\tpicture = \"to_tribal_integration_{i}\"\n')
-        f.write('}\n\n')
+    for index, image in enumerate(images):
+        for i in fractions:
+            f.write(f'{modifiers[index]["name"]}_{i} = {{\n')
+            f.write(f'\tpicture = \"{modifiers[index]["name"]}_{i}\"\n')
+            f.write('}\n\n')
+    # for i in fractions:
+    #     f.write(f'to_tribal_integration_{i} = {{\n')
+    #     f.write(f'\tpicture = \"to_tribal_integration_{i}\"\n')
+    #     f.write('}\n\n')
