482f7d00c65e53f92b975da9f50a28b5d8839446
Scurek <umajdek@gmail.com>
2022-07-17 02:34:27 +0200

    Elysian -> Roman/Elysian

--- scripts/code generation/exarchs/province_names.py
+++ scripts/code generation/exarchs/province_names.py
@@ -3,8 +3,8 @@ from shutil import copy2
 from os import path
 
 province_name_directory = "../../../deploy/common/province_names"
-original_file = "greek.txt"
-other_cultures = ["elysian_group", "spartakian_group"]
+original_file = "byzantine.txt"
+other_cultures = ["spartakian_group"]
 
 for tag in tags + other_cultures:
     copy2(path.join(province_name_directory, original_file), path.join(province_name_directory, tag + ".txt"))
